54f8c030ad0dd8150fa46c4dc85999a135410fcc
Mohannad Hisham <Mohannadize15@gmail.com>
2018-01-12 00:13:21 +0200

    Updated Overall Design

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-let swVer = 4;
+let swVer = 2;
 
 self.addEventListener("install",(e)=>{
 	e.waitUntil(
-		caches.open("Drama-1").then((cache)=>{
+		caches.open("Drama").then((cache)=>{
 			return cache.addAll(["./","./index.html","./drama.swf.mp3"]).then(()=>self.skipWaiting());
 		})
 	);
-})
+});
 self.addEventListener("fetch",(e)=>{
+	let request = e.request.url;
 	e.respondWith(
-		caches.match(e.request).then(function(cache){
-			return cache || fetch(e.request);
+		caches.match(e.request).then((res)=>{
+			return res || fetch(e.request).then((netres)=>{
+				return caches.open("Drama").then((cache)=>{
+					cache.put(request.url,netres.clone());
+					return netres;
+				});
+			});
 		})
 	);
 })
