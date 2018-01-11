1bbdd175da7d526ca2647d49dc181ad7e3e88f2d
Mohannad Hisham <Mohannadize15@gmail.com>
2018-01-11 20:59:58 +0200

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,16 @@
+let swVer = 1;
+
+self.addEventListener("install",(e)=>{
+	e.waitUntil(
+		caches.open("Drama-1").then((cache)=>{
+			return cache.addAll(["/","/index.html","/drama.swf.mp3"]).then(()=>self.skipWaiting());
+		})
+	);
+})
+self.addEventListener("fetch",(e)=>{
+	e.respondWith(
+		caches.match(e.request).then(function(cache){
+			return cache || fetch(e.request);
+		})
+	);
+})
