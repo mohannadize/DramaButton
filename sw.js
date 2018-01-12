572b6f5b7442cebb5a24f7b865ebfaa3b877f66f
Mohannad Hisham <Mohannadize15@gmail.com>
2018-01-12 16:04:07 +0200

    Updated Overall UI

--- sw.js
+++ sw.js
@@ -1,23 +1,25 @@
-const allCaches = ["Drama-1.0.2"];
+const allCaches = ["Drama-1.1"];
 self.addEventListener("install",(e)=>{
 	e.waitUntil(
 		caches.open(allCaches[0]).then((cache)=>{
-			return cache.addAll(["./","./index.html","./drama.swf.mp3","./about.html"]).then(()=>self.skipWaiting());
+			return cache.addAll(["./","./index.html","./drama.swf.mp3","./about.html","./manifest.json"]).then(()=>self.skipWaiting());
 		})
 	);
 });
 self.addEventListener("fetch",(e)=>{
 	let request = e.request.url;
-	e.respondWith(
-		caches.match(e.request).then((res)=>{
-			return res || fetch(e.request).then((netres)=>{
-				return caches.open(allCaches[0]).then((cache)=>{
-					cache.put(request,netres.clone());
-					return netres;
+	if (!request.startsWith("https://www.google-analytics.com") || !request.startsWith("https://www.googletagmanager.com")) {
+		e.respondWith(
+			caches.match(e.request).then((res)=>{
+				return res || fetch(e.request).then((netres)=>{
+					return caches.open(allCaches[0]).then((cache)=>{
+						cache.put(request,netres.clone());
+						return netres;
+					});
 				});
-			});
-		})
-	);
+			})
+		);
+	}
 });
 self.addEventListener("activate",(e)=>{
 	e.waitUntil(
